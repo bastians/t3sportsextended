d3caa2069301aa9beb71c7406c76a758fdab6558
bastians <bastians@live.de>
2019-04-17 11:52:19 +0200

    Some adjustments for TYPO3 v8

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
         'version' => '0.0.4',
         'constraints' => array(
                 'depends' => array(
-                        'typo3' => '4.5.0-7.6.99',
+                        'typo3' => '4.5.0-8.9.99',
                         'cfc_league' => '',
                         'cfc_league_fe' => '',
                 ),
--- ext_tables.sql
+++ ext_tables.sql
@@ -1,19 +1,16 @@
-#
+# 
 # Table structure for table 'tx_cfcleague_games'
 #
 CREATE TABLE tx_cfcleague_games (
-	tx_t3sportsextended_report_headline varchar(255) DEFAULT '' NOT NULL,
-	tx_t3sportsextended_report_subtitle varchar(255) DEFAULT '' NOT NULL,
-	tx_t3sportsextended_games_topgame tinyint(3) DEFAULT '0' NOT NULL
-	tx_t3sportsextended_games_coach_home_static varchar(255) DEFAULT '' NOT NULL,
-	tx_t3sportsextended_games_coach_guest_static varchar(255) DEFAULT '' NOT NULL,
+    tx_t3sportsextended_report_headline tinytext,
+    tx_t3sportsextended_report_subtitle tinytext,
+    tx_t3sportsextended_games_topgame tinyint(4) DEFAULT '0' NOT NULL,
+    tx_t3sportsextended_games_coach_home_static text,
+    tx_t3sportsextended_games_coach_guest_static text
 );
-
-
-
 #
 # Table structure for table 'tx_cfcleague_profiles'
 #
 CREATE TABLE tx_cfcleague_profiles (
-	tx_t3sportsextended_profiles_phone varchar(255) DEFAULT '' NOT NULL
+    tx_t3sportsextended_profiles_phone varchar(30) DEFAULT '' NOT NULL
 );
